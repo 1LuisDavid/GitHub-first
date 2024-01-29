c47d7c76352975ab350718ab109986962f3eaeb1
Luis David Ramirez Benitez <lrammr2@uic.edu>
2024-01-29 02:16:36 -0600

    Added some code

---
+++ Prueba.R
@@ -0,0 +1,8 @@
+Y = c(6/2, 7, 9*4, .1, 2044, 21)
+mean(x)
+median(x)
+sd(x)
+hist(x, main = "Histograma de mi objeto",
+     sub = "Mi primer vector",
+     ylab = "Frecuencia",
+     xlab = "categorías")
